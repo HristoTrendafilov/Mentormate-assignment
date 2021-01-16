49859c4e8e508c30b3108e2a4cee7d399a4c8a43
hristotrendafilov <hristo.trendafilov93@gmail.com>
2021-01-16 20:01:03 +0200

    fixed variable naming

--- Common/GlobalExceptions.cs
+++ Common/GlobalExceptions.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Common
+﻿namespace Common
 {
     public class GlobalExceptions
     {
-        public const string InvalidRowsNumber = "Invalid rows number.";
-        public const string InvalidRowsOrColsNumber = "Rows or cols should'nt exceed 100.";
+        public const string InvalidRowsOrColsNumber = "Invalid rows number.";
+        public const string ExceededRowsOrColsNumber = "Rows or cols should'nt exceed 100.";
         public const string InvalidBrickwork = "Invalid brickwork.";
     }
 }
