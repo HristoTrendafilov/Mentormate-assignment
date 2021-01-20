964670bdee083e030603140cdc7b35f25ada5ea9
hristotrendafilov <hristo.trendafilov93@gmail.com>
2021-01-20 17:36:38 +0200

    added more exception messages

--- Common/GlobalExceptions.cs
+++ Common/GlobalExceptions.cs
@@ -5,5 +5,7 @@
         public const string InvalidRowsOrColsNumber = "Invalid rows number.";
         public const string ExceededRowsOrColsNumber = "Rows or cols should'nt exceed 100.";
         public const string InvalidBrickwork = "Invalid brickwork.";
+        public const string NumbersOnRowExceeded = "Numbers on row exceeded.";
+        public const string InvalidNumbersCount = "Invalid number added on row {0}.";
     }
 }
